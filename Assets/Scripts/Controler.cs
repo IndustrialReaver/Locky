4a2ce4846db6e0732a2831e6e0a7596eccf2cec0
Ben <industrialreaver@gmail.com>
2015-10-29 14:51:15 -0400

    initial commit

---
+++ Assets/Scripts/Controler.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controler : MonoBehaviour {
+
+    public GameObject Bar;
+    public GameObject Dot;
+
+    public float speed = 5;
+    public int score = 1;
+
+
+	// Use this for initialization
+	void Start () {
+        Bar.transform.rotation.Set(0,0,90,0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    Bar.transform.Rotate(Vector3.forward, speed * Time.deltaTime);
+	}
+}
